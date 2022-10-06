a4ff25b494f11a19a5c465c4c60549f2fef525ab
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-10-06 15:14:04 +0900

    Update 없는 숫자 더하기.java

--- Programmers/Level 1/없는 숫자 더하기.java
+++ Programmers/Level 1/없는 숫자 더하기.java
@@ -2,10 +2,6 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         boolean[] bool=new boolean[10];
-        for(int i=0;i<bool.length;i++)
-        {
-            bool[i]=false;    
-        }
         for(int i=0;i<numbers.length;i++)
         {
             bool[numbers[i]]=true;    
