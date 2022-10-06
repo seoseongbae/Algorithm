4272adbba521d37188f38c72004d521f3282076f
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-10-06 14:59:08 +0900

    Create 음양 더하기.java

---
+++ Programmers/Level 1/음양 더하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0;i<absolutes.length;i++)
+        {
+            if(signs[i]==false)
+            {
+                absolutes[i]=absolutes[i]*-1;   
+            }
+            else{
+                absolutes[i]=Math.abs(absolutes[i]);
+            }
+        }
+        for(int i=0;i<absolutes.length;i++)
+        {
+            answer+=absolutes[i];    
+        }
+        return answer;
+    }
+}
