e01f94b59811c17c6925f08c141968f8ba8ac3c2
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-11-10 02:31:32 +0900

    Create 숫자 문자열과 영단어.java

---
+++ Programmers/Level 1/숫자 문자열과 영단어.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer=0;
+        String[] word= {"zero" , "one" , "two" , "three" , "four" , "five" , "six" , "seven" , "eight" , "nine"};
+        for(int i=0;i<10;i++)
+        {
+            s=s.replace(word[i],Integer.toString(i));    
+        }
+        answer=Integer.parseInt(s);
+        return answer;
+    }
+}
