2ced49de72c5d15225dc398a0fbbaa4a05bbf00c
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-12-22 12:58:08 +0900

    Create 2016년.java

---
+++ Programmers/Level 1/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] date = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int dd = 0;
+
+        for(int i = 0; i < a-1; i++){
+            dd += date[i];
+        }
+        dd += b - 1;
+
+        return day[dd % 7];
+    }
+}
