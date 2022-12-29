e21c340a3be6ebe86b61306ebbbbe2ef79e93477
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-12-29 12:52:55 +0900

    Create 최대값과 최소값.java

---
+++ Programmers/Level 2/최대값과 최소값.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str=s.split(" ");
+        int min,max,n;
+        min=max=Integer.parseInt(str[0]);
+        for (int i = 1; i < str.length; i++) {
+            n = Integer.parseInt(str[i]);
+            if(min > n) min = n;
+            if(max < n) max = n;
+        }
+        return min+" "+max;
+    }
+}
