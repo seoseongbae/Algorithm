517ee9af3a24b368354f7821589055ac2225cd56
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2022-12-29 13:02:57 +0900

    Create JadenCase 문자열 만들기.java

---
+++ Programmers/Level 2/JadenCase 문자열 만들기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String str[]=s.split(" ");
+        for(int i=0; i<str.length; i++) {
+    		String now = str[i];
+    		
+    		if(str[i].length() == 0) {
+    			answer += " ";
+    		} 
+    		else {
+    			answer += now.substring(0, 1).toUpperCase();
+    			answer += now.substring(1, now.length()).toLowerCase();
+    			answer += " ";
+    		}
+    		
+    	}
+        if(s.substring(s.length() -1, s.length()).equals(" "))
+        {
+            return answer;
+        }
+        return answer.substring(0,answer.length()-1);
+    }
+}
