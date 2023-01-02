e97dab9850037d0a77e5bea5fac4bd827098af8b
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2023-01-02 21:48:32 +0900

    Update 올바른 괄호.java

--- Programmers/Level 2/올바른 괄호.java
+++ Programmers/Level 2/올바른 괄호.java
@@ -1,1 +1,28 @@
-
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        String[]str=s.split("");
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                stack.push('(');    
+            }
+            else{
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
+            }
+        }
+        answer=(stack.isEmpty())?true:false;
+        
+        return answer;
+    }
+}
