573eddcc843714cd871a0869f9bb13023e23ce56
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-03-28 23:58:18 +0900

    Update 하샤드 수.java

--- Programmers/Level 1/하샤드 수.java
+++ Programmers/Level 1/하샤드 수.java
@@ -18,3 +18,18 @@ class Solution {
         }
     }
 }
+--------------------------------------------------------
+다시 푼 문제 - int형을 string 형으로 반환 후 문자로 하나씩 받아 계산.
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum=0;
+		String str=String.valueOf(x);
+		for(int i=0;i<str.length();i++) {
+			sum+=str.charAt(i)-48;
+		}
+		if(x%sum==0) answer=true;
+        else answer=false;
+        return answer;
+    }
+}
