27a4dce2fd85b918dd02383c9a7f46bd4d9b8781
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-04-05 19:23:11 +0900

    Update 제일 작은 수 제거하기.java

--- Programmers/Level 1/제일 작은 수 제거하기.java
+++ Programmers/Level 1/제일 작은 수 제거하기.java
@@ -29,3 +29,35 @@ class Solution {
     return answer;
     }
 }
+-------------------------------------------------
+다시 푼 코드
+    class Solution {
+    public int[] solution(int[] arr) {
+       int[] answer;
+		int min=arr[0];
+		if(arr.length>1) {
+			answer=new int[arr.length-1];
+			for(int i=0;i<arr.length;i++)
+			{
+				if(min>arr[i]) {
+					min=arr[i];
+				}
+			}
+			int count=0;
+			for(int i=0;i<arr.length;i++)
+			{
+				if(arr[i]==min) {
+					continue;
+				}
+				else {
+					answer[count++]=arr[i];
+				}
+			}	
+		}else {
+			answer=new int[1];
+			answer[0]=-1;
+		}
+		
+        return answer;
+    }
+}
