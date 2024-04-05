cd0e353191f6bb8ef54fdfb4ec711b75bbcbc9e4
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-04-05 14:01:44 +0900

    Update 콜라츠 추측.java

--- Programmers/Level 1/콜라츠 추측.java
+++ Programmers/Level 1/콜라츠 추측.java
@@ -28,3 +28,26 @@ class Solution {
         return answer;
     }
 }
+-----------------------------------------------------------------------------------------
+다시 푼 소스
+    class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        while(num>1){
+            if(num%2==1){
+                num=num*3+1;
+                    answer++;
+            }
+            else if(num%2==0){
+                num=num/2;
+                answer++;
+                
+            }
+            if(answer==500){
+                answer=-1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
