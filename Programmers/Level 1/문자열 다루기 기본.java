b29a7eb44b30b148567f4f64d60fc3f48794f076
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-04-19 12:12:14 +0900

    Update 문자열 다루기 기본.java

--- Programmers/Level 1/문자열 다루기 기본.java
+++ Programmers/Level 1/문자열 다루기 기본.java
@@ -17,3 +17,24 @@ class Solution {
         return answer;
     }
 }
+---------------------------------------------------
+다시 푼 코드
+    class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        int count=0;
+        if(s.length()==4||s.length()==6){
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i) < 48 || s.charAt(i) > 57)
+                {
+                    count++;
+                }
+            }
+            if(count!=0) answer=false; 
+        }else{
+            answer=false;
+        }
+       
+        return answer;
+    }
+}
