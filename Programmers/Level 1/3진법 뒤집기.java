ddfa629ae59d5ec8453a95ec6fa01f06eae4b974
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-04-27 16:59:32 +0900

    Update 3진법 뒤집기.java

--- Programmers/Level 1/3진법 뒤집기.java
+++ Programmers/Level 1/3진법 뒤집기.java
@@ -18,3 +18,31 @@ class Solution {
         return answer;
     }
 }
+--------------------------
+새로운 코드 -
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+       int count=n;
+		String str="";
+		while(true) {
+			str+=String.valueOf(n%3);
+			n=n/3;
+			if(n/3<1) {
+				str+=n;
+				System.out.println(str);
+				break;
+			}
+		}
+		for(int i=0;i<str.length();i++)
+		{
+			if(count/3<3) {
+				answer+=Math.pow(3, i)*(str.charAt(i)-48);				
+			}
+			else {
+				answer+=Math.pow(3, i)*(str.charAt(str.length()-i-1)-48);
+			}
+		}
+        return answer;
+    }
+}
