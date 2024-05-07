c2a11743f85ce35e911ffe0c10be2bf1b84491b5
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-05-07 21:12:54 +0900

    Update 예산.java

--- Programmers/Level 1/예산.java
+++ Programmers/Level 1/예산.java
@@ -18,3 +18,23 @@ class Solution {
          return answer;   
     }
 }
+----------------------------------------------------
+최대한 많은 부서에 예산을 지급해야하므로 budget보다 적게 부서에 많이 지급해야함으로
+sort후 sum값에 더한뒤 budget보다 적을떄까지 count해준다.
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum=0;
+        Arrays.sort(d);
+        for(int i=0;i<d.length;i++){
+            sum+=d[i];
+            if(sum<=budget){
+              answer++;  
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
