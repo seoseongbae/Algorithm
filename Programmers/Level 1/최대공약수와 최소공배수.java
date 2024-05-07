806262ba882b4476573a80ac9529196b46471706
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-05-07 21:05:30 +0900

    Update 최대공약수와 최소공배수.java

--- Programmers/Level 1/최대공약수와 최소공배수.java
+++ Programmers/Level 1/최대공약수와 최소공배수.java
@@ -16,3 +16,23 @@ class Solution {
 		}
     }
 }
+--------------------------------------------------
+유클리드 호제법사용하여 작성.
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        answer[0]=gcd(n,m);
+		answer[1]=n*m/answer[0];
+        return answer;
+    }
+    static int gcd(int n,int m) {
+		int r;
+		while(m>0) {
+			r=n%m;
+			n=m;
+			m=r;
+		}
+		return n;
+		
+	}
+}
