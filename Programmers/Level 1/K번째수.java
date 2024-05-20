3211a4b456d975ec1dda7d5dd5a4006101279d89
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-05-20 12:44:36 +0900

    Update K번째수.java

--- Programmers/Level 1/K번째수.java
+++ Programmers/Level 1/K번째수.java
@@ -1,17 +1,17 @@
+//list에 필요한 데이터를 추가후 정렬하여 원하는 값 추출한다.
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        answer=new int[commands.length];
-        for(int i=0;i<commands.length;i++)
-        {
-           ArrayList<Integer> list=new ArrayList<Integer>();
-            for(int j=commands[i][0]-1;j<commands[i][1];j++)
-            {
-                list.add(array[j]);    
+        List list=new ArrayList();
+		int[] answer = new int [commands.length];
+        int count=0;
+        for(int i=0;i<commands.length;i++){
+            for(int j=commands[i][0]-1;j<=commands[i][1]-1;j++){
+              list.add(array[j]);
             }
             Collections.sort(list);
-            answer[i]=list.get(commands[i][2]-1);
+            answer[count++]=(int) list.get(commands[i][2]-1);
+            list.clear();
         }
         
         return answer;
