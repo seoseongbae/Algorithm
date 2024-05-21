7cb58029a227a1198976d8431fb26e0ed56c0a74
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-05-21 10:20:58 +0900

    Update 콜라 문제.java

--- Programmers/Level 1/콜라 문제.java
+++ Programmers/Level 1/콜라 문제.java
@@ -1,15 +1,16 @@
+다시 푼 문제 n개중 a개당 b개씩 준다. 그러므로 n에a를 나눈값에 b를 곱해줘야 한다.
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        
-        while(true)
-        {
-            if(n<a)
-            {
+        while(true){
+          if(n<a||n<2){
                 break;
             }
-            answer+=(n/a)*b;
-            n=((n/a*b)+(n%a));
+
+            answer+=n/a*b;
+
+            n=((n/a)*b)+(n%a);
+
         }
         return answer;
     }
