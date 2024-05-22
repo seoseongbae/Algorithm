3159e813195bf4c93ac1c85a11a9693e5c363061
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-05-22 09:32:11 +0900

    Update 폰켓몬.java

--- Programmers/Level 1/폰켓몬.java
+++ Programmers/Level 1/폰켓몬.java
@@ -1,19 +1,19 @@
+------
+hashset을 이용하여 값 존재여부 확인하여 없으면 값을 넣어 그 값에 대한 길이를 반환
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int max=nums.length/2;
-        int answer = 0;
-        HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            set.add(nums[i]);
-        }
-        if(max>=set.size())
-        {
-            answer=set.size();    
-        }else{
-           answer=max;
-        }
+       int answer=0;
+		HashSet<Integer> hash =new HashSet<Integer>();
+		for(int i=0;i<nums.length;i++) {
+			if(!hash.contains(nums[i]))
+			hash.add(nums[i]);
+		}
+		if(hash.size()>nums.length/2) {
+			answer=nums.length/2;
+		}else {
+			answer= hash.size();
+		}
         return answer;
     }
 }
