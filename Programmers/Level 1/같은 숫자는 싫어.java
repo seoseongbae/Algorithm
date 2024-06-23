b871cfa65fefed08787a2d5e0251c8df94fb38b4
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-06-23 19:20:42 +0900

    Update 같은 숫자는 싫어.java

--- Programmers/Level 1/같은 숫자는 싫어.java
+++ Programmers/Level 1/같은 숫자는 싫어.java
@@ -13,9 +13,34 @@ public class Solution {
                  for (int j = 0; j < arrlist.size(); j++){
              answer[j] = arrlist.get(j);
 
-         }
-
-
+        }
         return answer;
     }
 }
+
+-------------------------------------------------------------------------
+다시 푼 문제
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+		List list = new ArrayList();
+		
+		for(int i=0;i<arr.length;i++) {
+			if(i==0) {
+				list.add(arr[i]);
+			}else {
+				if(arr[i-1]!=arr[i]) {
+					list.add(arr[i]);
+				}
+			}
+		}
+		int answer[]=new int[list.size()];
+		for(int i=0;i<answer.length;i++) {
+			answer[i]=(int) list.get(i);
+		}
+        
+
+        return answer;
+    }
+} 
