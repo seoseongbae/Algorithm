8f91f0abd5b08c712bb9196c3e8af55b668f5fa3
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-06-24 15:02:33 +0900

    Update 시저 암호.java

--- Programmers/Level 1/시저 암호.java
+++ Programmers/Level 1/시저 암호.java
@@ -27,3 +27,30 @@ class Solution {
         return answer;
     }
 }
+------------------------------------------------------------------------------------
+
+	class Solution {
+    public String solution(String s, int n) {
+       String answer="";
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i)==' ') {
+				answer+=' ';
+			}else {
+				if (s.charAt(i) != 'Z' && s.charAt(i) != 'z') { //문자가 Z,z가 아닐경우
+					if(s.charAt(i)<'Z'&& s.charAt(i)+n>'Z'||s.charAt(i)+n>'z') {//마지막 값 넘길경우
+						answer+=(char)(s.charAt(i)+n-26); //초기 a,A로 바꿔서 추가로 넘겨줌
+					}else {
+						answer += (char) (s.charAt(i) + n); //아닐경우 그냥 +n만큼 해줌
+					}
+				}else {
+					if (s.charAt(i)=='Z') { //Z일경우 
+						answer += (char)('A'+n-1); //A로 넘어가야함으로 A로변환한후+n만큼옮기고-1
+					} else if (s.charAt(i)=='z') {
+						answer += (char)('a'+n-1);
+					}
+				}
+			}
+		}
+        return answer;
+    }
+}
