9b759e810ad512b195356e6238664e1fde94ae68
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-07-02 13:41:44 +0900

    Create 콜라츠 수열 만들기.java

---
+++ Programmers/Level 0/콜라츠 수열 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        List list =new ArrayList();
+        list.add(n);
+        while(n!=1){
+            if(n%2==0){
+                n=n/2;
+                list.add(n);
+            }else{
+                n=3*n+1;
+                list.add(n);
+            }
+        }
+        answer=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]=(int)list.get(i);
+        }
+        return answer;
+    }
+}
