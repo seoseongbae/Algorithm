646e4e03ad5fe824659750f0bc80c0c7dc4f01a3
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-07-05 00:09:43 +0900

    Update 문자열이 몇 번 등장하는지 세기.java

--- Programmers/Level 0/문자열이 몇 번 등장하는지 세기.java
+++ Programmers/Level 0/문자열이 몇 번 등장하는지 세기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
         for(int i=0;i<=myString.length()-pat.length();i++){
-            if(myString.substring(i,i+pat.length()).equals(pat)){//3자리씩 자른 문자열이 pat과 같은지 비교
+            if(myString.substring(i,i+pat.length()).equals(pat)){//pat의 길이만큼 자른 문자열이 pat과 같은지 비교
                 answer++;
             }
         }
