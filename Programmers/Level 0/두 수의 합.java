8e57f50601c3ce735723f3a719d88cc9452bf910
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-07-05 19:09:10 +0900

    Update 두 수의 합.java

--- Programmers/Level 0/두 수의 합.java
+++ Programmers/Level 0/두 수의 합.java
@@ -6,3 +6,14 @@ class Solution {
         
     }
 }
+
+---------------------------------------------------
+import java.math.*;
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger c = new BigInteger(a);
+		BigInteger d = new BigInteger(b);
+		BigInteger max = c.add(d);
+		String answer = max.toString();
+        return answer;
+    }
