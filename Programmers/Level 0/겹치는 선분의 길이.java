90a8a69a534c46537d5c5d969f788212022c91c1
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-07-13 23:09:29 +0900

    Create 겹치는 선분의 길이.java

---
+++ Programmers/Level 0/겹치는 선분의 길이.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        
+        int count[] = new int[201];
+        
+        for(int i=0;i<lines.length;i++){
+            int start = lines[i][0]+100; //음수를 해결하기위해 +100을 하여 범위를 0부터 200으로 처리
+            int end = lines[i][1]+100;
+            
+            for(int j=start;j<end;j++){
+                count[j]+=1;
+            }
+        }
+        
+        for(int i=0;i<count.length;i++){
+            if(count[i]>1){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
