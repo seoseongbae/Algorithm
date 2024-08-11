60a8acb86af8d3d851b1bfbbeb998947d8ed066b
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-11 23:23:16 +0900

    Update 최대값과 최소값.java

--- Programmers/Level 2/최대값과 최소값.java
+++ Programmers/Level 2/최대값과 최소값.java
@@ -13,3 +13,20 @@ class Solution {
         return min+" "+max;
     }
 }
+-----------------------------------------------------
+//다른 풀이
+    import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String [] str = s.split(" ");
+		int [] arr = new int[str.length];
+		for(int i=0;i<str.length;i++) {
+			arr[i]=Integer.parseInt(str[i]);
+		}
+		Arrays.sort(arr);
+		answer = arr[0] + " " + arr[arr.length-1];
+		
+        return answer;
+    }
+}
