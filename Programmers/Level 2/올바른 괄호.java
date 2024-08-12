e0ed8987c8f35138d50b31d6a556539c32604b38
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-12 21:43:23 +0900

    Update 올바른 괄호.java

--- Programmers/Level 2/올바른 괄호.java
+++ Programmers/Level 2/올바른 괄호.java
@@ -26,3 +26,28 @@ class Solution {
         return answer;
     }
 }
+------------------------------------------------------
+//다시 풀어본 코드
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack =new Stack<Character>();
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)=='(') {  //문자가 '('일 경우 스택에 추가
+				stack.push(s.charAt(i));
+			}else if(s.charAt(i)==')') { //문자가 ')'일 경우 
+				if(stack.isEmpty()) { //스택안에가 비어있다면
+					answer=false; // answer = false로 반환
+					break;
+				}
+				stack.pop(); // 스택에서 제거
+			}
+		}
+        if(!stack.isEmpty()) { //스택안에가 비어있지 않다면 false;
+                    answer=false; 
+        }
+        return answer;
+    }
+}
