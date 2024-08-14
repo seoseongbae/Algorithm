09e670e29567fb9ac6c03b0a676d47166daaefa0
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-14 17:10:46 +0900

    Update 최솟값 만들기.java

--- Programmers/Level 2/최솟값 만들기.java
+++ Programmers/Level 2/최솟값 만들기.java
@@ -13,3 +13,25 @@ class Solution
         return answer;
     }
 }
+-------------------------------------------------------
+//다시푼 코드
+import java.util.*;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        
+        Arrays.sort(A);
+		List list = new ArrayList();
+		for(int i=0;i<B.length;i++) {
+			list.add(B[i]);
+		}
+		Collections.sort(list, Collections.reverseOrder()); //역순으로 정렬
+		for(int i=0;i<A.length;i++) {
+			answer +=A[i]*(int)list.get(i);
+		}
+
+        return answer;
+    }
+}
