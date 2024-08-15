680f8a6de0b87d89f56b47db716b927ba308c3e1
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-15 23:17:40 +0900

    Update 이진 변환 반복하기.java

--- Programmers/Level 2/이진 변환 반복하기.java
+++ Programmers/Level 2/이진 변환 반복하기.java
@@ -24,3 +24,30 @@ class Solution {
         return answer;
     }
 }
+----------------------------------------------------
+//다시 푼 문제
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int count=0; //몇번 돌아갔는지 확인하는 함수
+		int delzero=0; //0이 몇개 제거되었는지 확인하는 함수
+		int slen = 0;//길이 담는 변수
+		while(true) {
+			for(int i=0;i<s.length();i++) {
+				if(s.charAt(i)=='0') {
+					delzero++;
+				}
+			}
+			s = s.replaceAll("0", "");
+			slen = s.length();
+			count++;
+			s = Integer.toBinaryString(slen);
+			if(s.equals("1")) {
+				break;
+			}
+		}
+		answer[0]=count;
+		answer[1]=delzero;
+        return answer;
+    }
+}
