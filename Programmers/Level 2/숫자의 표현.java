7361f0bf0e63ce24f137137659579712bb4c15d3
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-16 15:38:07 +0900

    Update 숫자의 표현.java

--- Programmers/Level 2/숫자의 표현.java
+++ Programmers/Level 2/숫자의 표현.java
@@ -17,3 +17,31 @@ class Solution {
         return answer;
     }
 }
+------------------------------------------
+//다시 푼 코드
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i=1;
+		int sum=0;
+		while(true) {
+			for(int j=i;j<=n;j++) {
+				sum+=j;	
+				if(sum==n) {
+					answer++;
+					break;
+				}
+				if(sum>n) { //sum 값이 n보다 커지면 시간을 많이 잡아먹어 커지면 break로 for문을 벗어나게함.
+					break;
+				}
+			}
+			if(i==n) {
+				break;
+			}
+			i++;
+			sum=0;
+			
+		}
+        return answer;
+    }
+}
