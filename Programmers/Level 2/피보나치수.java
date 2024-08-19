2f16b5899fea0c4c802c6f22fd01408b7d17a1d0
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-19 10:59:21 +0900

    Update 피보나치수.java

--- Programmers/Level 2/피보나치수.java
+++ Programmers/Level 2/피보나치수.java
@@ -20,3 +20,23 @@ class Solution {
         return answer[n];
 }
 }
+------------------------------------------------
+다시 푼 문제
+//재귀 함수로 호출할 경우 중복연산과 불필요한 메모리 사용을 하여 시간초과 실패가 떠 동적계획법을 이용하여 풀이
+class Solution {
+    public int solution(int n) {
+        int answer = fn(n);
+        return answer;
+    }
+    
+    
+    public int fn(int n){
+       int[] arr = new int[n+1];
+		arr[0]=0;
+		arr[1]=1;
+		for(int i=2;i<=n;i++) {
+			arr[i] = (arr[i-1]+arr[i-2])%1234567;
+		}
+		return arr[n];
+    }
+}
