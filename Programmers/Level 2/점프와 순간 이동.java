0c9fa74eae93b40a1c1cf2a9e5a7b78ac9d1fde3
seoseongbae <86153377+seoseongbae@users.noreply.github.com>
2024-08-24 17:59:46 +0900

    Update 점프와 순간 이동.java

--- Programmers/Level 2/점프와 순간 이동.java
+++ Programmers/Level 2/점프와 순간 이동.java
@@ -1,1 +1,19 @@
 //점프와 순간 이동
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n!=0){
+            if(n%2==0){//순간이동 할 경우
+                n/=2;
+            }else{//점프할 경우
+                n--;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
