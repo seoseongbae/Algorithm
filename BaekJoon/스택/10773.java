c5b43c9b50557790a2c71061744df80e626bd39c
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-23 10:25:33 +0900

    Update 10773.java

--- BaekJoon/스택/10773.java
+++ BaekJoon/스택/10773.java
@@ -27,4 +27,43 @@ public class Main {
 		System.out.println(sum);
 	}
 }
+------------------------------------------------------------------------
+#다시 푼 문제
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            
+            if(!stack.isEmpty()) {
+            	if(num == 0 ) {
+            		stack.pop();
+            	} else {
+            		stack.push(num);
+            	}
+            } else {
+            	stack.push(num);
+            }
+     
+        }
+        int sum = 0;
+        while(true) {
+        	if(stack.isEmpty()) {
+        		break;
+        	}
+        	sum += stack.pop();
+        }
+        System.out.print(sum);
+	}
+}
