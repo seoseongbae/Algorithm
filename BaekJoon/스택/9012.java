664abda96972c5bef2f070c803ebf2a515ee2667
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-25 17:11:10 +0900

    Update 9012.java

--- BaekJoon/스택/9012.java
+++ BaekJoon/스택/9012.java
@@ -43,3 +43,50 @@ public class Main {
 	}
 }
 
+---------------------------------------------------------------
+#다시 푼 문제
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); 
+        
+        for(int i=0;i<n;i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	String str = st.nextToken();
+        	Stack<String> stack = new Stack<String>();
+        	
+        	String[] s = str.split("");
+        	
+        	for (int j=0; j<s.length; j++) {
+        		if(s[j].equals("(")) {
+        			stack.push(s[j]);
+        		} else {
+        			if(!stack.isEmpty()) {
+        				if(stack.peek().equals("(")) {
+        					stack.pop();
+        				} else {
+        					stack.push(s[j]);
+        				}
+        			} else {
+        				stack.push(s[j]);
+        			}
+        		}
+        	}                                                                                                                                                                                         
+        	
+        	if(stack.isEmpty()) {
+        		System.out.println("YES");
+        	} else {
+        		System.out.println("NO");
+        	}
+        	
+        }
+	}
+}
