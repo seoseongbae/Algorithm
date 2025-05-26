aa1aa6d61f489284b60b1414ee6d939d2c2fa7f2
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-26 10:55:48 +0900

    Update 10828.java

--- BaekJoon/스택/10828.java
+++ BaekJoon/스택/10828.java
@@ -78,4 +78,49 @@ public class Main {
 		}
 	}
 }
+----------------------------------------------------------
+#다시 푼 문제
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+        	Stack<Integer> stack = new Stack<Integer>();
+		for (int i=0; i<num; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			switch (st.nextToken()) {
+			case "push":
+				stack.push(Integer.parseInt(st.nextToken()));
+				break;
+			case "pop":
+				if(stack.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(stack.peek());
+					stack.pop();
+				}
+				break;
+			case "size":
+				System.out.println(stack.size());
+				break;
+			case "empty":
+				if(stack.isEmpty()) {
+					System.out.println("1");
+				} else {
+					System.out.println("0");
+				}
+				break;
+			case "top":
+				if(stack.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(stack.peek());
+				}
+				break;
+			}
+		}
+	}
+}
 
