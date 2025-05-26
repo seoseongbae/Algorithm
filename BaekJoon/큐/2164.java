cc3503c61a408498ef755186483c2d03c25bac44
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-26 14:43:04 +0900

    Update 2164.java

--- BaekJoon/큐/2164.java
+++ BaekJoon/큐/2164.java
@@ -20,3 +20,31 @@ public class Main {
 		System.out.println(q.poll());
 	}
 }
+------------------------------------------------------------------------------
+#다시 풀어봄
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> que = new LinkedList<>();
+		
+		for(int i=1;i<=num;i++) {
+			que.add(i);
+		}
+		while(que.size()!=1) {
+			//버리기
+			que.poll();
+			
+			//뒤로 보내기
+			int n = que.poll();
+			que.offer(n);
+			
+		}
+		System.out.println(que.poll());
+	}
+}
