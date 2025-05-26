e258bca5a89220be376366a84aa918e88a013756
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-26 09:59:02 +0900

    Update 4949.java

--- BaekJoon/스택/4949.java
+++ BaekJoon/스택/4949.java
@@ -50,3 +50,56 @@ public class Main {
 	}
 
 }
+------------------------------------------------------------------------------
+#다시 풀어본 문제
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        while(true) {
+	        	String str = br.readLine();
+	        	if(str.equals(".")) {
+	        		break;
+	        	}
+	
+	        	String res = str.replaceAll("[^\\[\\]()]", "");
+	        	if(res.length()==0) {
+	        		System.out.println("yes");
+	        	} else {
+		        	String[] arrSt = res.split("");
+		        	Stack<String> stack = new Stack<String>();
+		        	for (String s : arrSt) {
+		        		if(stack.isEmpty()) {
+		        			stack.push(s);
+		        		} else {
+		        			if(s.equals(")")) {
+		        				if(stack.peek().equals("(")) {
+		        					stack.pop();
+		        				} else {
+		        					stack.push(s);
+		        				}
+		        			} else if(s.equals("]")) {
+		        				if (stack.peek().equals("[")) {
+		        					stack.pop();
+		        				} else {
+		        					stack.push(s);
+		        				}
+		        			} else {
+		        				stack.push(s);
+		        			}
+		        		}
+				}
+		        	if(stack.isEmpty()) {
+		        		System.out.println("yes");
+		        	} else {
+		        		System.out.println("no");
+		        	}
+	        	}
+	        }
+	}
+}
