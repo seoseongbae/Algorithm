c604b555973b030385bc352a8594a4db870498a2
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-28 14:27:03 +0900

    Create 4153.java

---
+++ BaekJoon/수학/4153.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			if(a==0&&b==0&&c==0) {
+				break;
+			}
+			
+			if(Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2) || Math.pow(a, 2) + Math.pow(c, 2) == Math.pow(b, 2) || Math.pow(c, 2) + Math.pow(b, 2) == Math.pow(a, 2)) {
+				System.out.println("right");
+			} else {
+				System.out.println("wrong");
+			}
+		}
+	}
+
+}
