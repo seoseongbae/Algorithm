993c92d3210ce0681314ae48015d312806f7ff58
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-29 17:08:56 +0900

    Update 1920.java

--- BaekJoon/이분탐색/1920.java
+++ BaekJoon/이분탐색/1920.java
@@ -60,3 +60,43 @@ public class Main {
  
 	}
 }
+---------------------------------------------------------------------
+#다시 풀어본 문제
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); 
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int[] arr= new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int m = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i=0;i<m;i++) {
+			int num = Integer.parseInt(st.nextToken());
+
+            int val = Arrays.binarySearch(arr, num);
+
+            if (val < 0) {
+                sb.append(0 + "\n");
+            } else {
+                sb.append(1 + "\n");
+            }
+			
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+}
