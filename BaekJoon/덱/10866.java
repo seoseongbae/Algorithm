ba2df517e2e4798bd24f1b7787e1e43b47191320
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-05-30 22:23:09 +0900

    Update 10866.java

--- BaekJoon/덱/10866.java
+++ BaekJoon/덱/10866.java
@@ -63,3 +63,71 @@ public class Main {
 	        System.out.println(sb);
 	}
 }
+-------------------------------------------------------------------------------
+#다시 푼 문제
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		Deque<Integer> deq = new ArrayDeque<Integer>();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			switch (st.nextToken()) {
+			case "push_front":
+				deq.offerFirst(Integer.parseInt(st.nextToken()));
+				break;
+			case "push_back":
+				deq.offerLast(Integer.parseInt(st.nextToken()));
+				break;
+			case "pop_front":
+				int val1 = -1;
+				if (!deq.isEmpty()) {
+					val1 = deq.pollFirst();
+				}
+				System.out.println(val1);
+				break;
+			case "pop_back":
+				int val2 = -1;
+				if (!deq.isEmpty()) {
+					val2 = deq.pollLast();
+				}
+				System.out.println(val2);
+				break;
+			case "size":
+				System.out.println(deq.size());
+				break;
+			case "empty":
+				int val3 = 0;
+				if (deq.isEmpty()) {
+					val3 = 1;
+				}
+				System.out.println(val3);
+				break;
+			case "front":
+				int val4 = -1;
+				if (!deq.isEmpty()) {
+					val4 = deq.peekFirst();
+				}
+				System.out.println(val4);
+				break;
+			case "back":
+				int val5 = -1;
+				if (!deq.isEmpty()) {
+					val5 = deq.peekLast();
+				}
+				System.out.println(val5);
+				break;
+			}
+
+		}
+	}
+}
