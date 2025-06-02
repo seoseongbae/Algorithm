e2173abe525321908a6cbc088360d640c9f5e833
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-06-02 09:45:13 +0900

    Update 1874.java

--- BaekJoon/스택/1874.java
+++ BaekJoon/스택/1874.java
@@ -35,4 +35,46 @@ public class Main {
 		System.out.println(sb);
 	}
 }
+---------------------------------------------------------------------
+#다시푼문제
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); 
+		
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		int start = 0;
+	
+		while(n-- > 0) {
+			
+			int value = Integer.parseInt(br.readLine());
+			
+			if(value > start) {
+				for(int i = start + 1; i <= value; i++) {
+					stack.push(i);
+					sb.append("+\n");
+				}
+				start = value; 	
+			} else if(stack.peek() != value) {
+				System.out.println("NO");
+				return;
+			}
+			
+			stack.pop();
+			sb.append("-\n");
+		}
+		System.out.println(sb.toString());
+
+	}
+}
