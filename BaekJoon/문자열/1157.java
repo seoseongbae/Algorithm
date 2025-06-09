d9c15931deba4527b207ed0f49afd7fa61cd4521
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-06-09 16:36:41 +0900

    Update 1157.java

--- BaekJoon/문자열/1157.java
+++ BaekJoon/문자열/1157.java
@@ -32,3 +32,35 @@ public class Main {
 		System.out.println(ch);
 	}
 }
+--------------------------------------------------------------------------
+#다시 푼 문제
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		s= s.toLowerCase();
+		int[] arr = new int[26];
+		for(int i=0;i<s.length();i++) {
+			arr[s.charAt(i)-'a'] +=1;
+		}
+		int max = -1;
+		char ch = '?';
+ 
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i + 65);
+			}
+			else if (arr[i] == max) {
+				ch = '?';
+			}
+		}
+		System.out.print(ch);
+	}
+	
+}
