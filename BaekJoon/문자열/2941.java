88a1804985aec0a2915c669f803c1a257c46b740
성배 <86153377+seoseongbae@users.noreply.github.com>
2025-06-10 09:48:16 +0900

    Update and rename 2941 to 2941.java

--- BaekJoon/문자열/2941.java
+++ BaekJoon/문자열/2941.java
@@ -74,3 +74,27 @@ public class Main {
 	}
 
 }
+----------------------------------------------------------------------
+#다시 푼 문제
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		String[] str = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+		
+		for(int i=0;i<str.length;i++) {
+			if(s.contains(str[i])) {
+				s=s.replace(str[i], "@");
+			}
+		}
+
+		System.out.println(s.length());
+		
+	}
+	
+}
